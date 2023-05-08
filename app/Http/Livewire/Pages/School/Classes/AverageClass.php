4c015c4006e68eb910199ccf1163c9eb07766807
bayupb <kampuspb.bayu@gmail.com>
2023-05-08 21:27:47 +0700

    add new component

---
+++ app/Http/Livewire/Pages/School/Classes/AverageClass.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\School\Classes;
+
+use Livewire\Component;
+
+class AverageClass extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.school.classes.average-class');
+    }
+}
